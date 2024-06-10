26e6ccd07bebb179b75e9d4ccdc78ef840314ddd
CynicalMrJones <wopjr1234@gmail.com>
2024-06-09 22:42:21 -0400

    Assignment 2 done

--- assignment2/assignment2.c
+++ assignment2/assignment2.c
@@ -1,9 +1,12 @@
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define ARRAYSIZE 20
 
-void bubbleSort(char lastName[ARRAYSIZE][50], char firstName[ARRAYSIZE][50], int age[]);
+void bubbleSort(char lastName[ARRAYSIZE][50], char firstName[ARRAYSIZE][50], int age[], int numberToList);
+void swap(int* i,int* j);
 
 int main(){
     char firstName[ARRAYSIZE][50];
@@ -16,13 +19,38 @@ int main(){
         printf("Please enter last name, first name, and age: ");
         scanf("%s %s %d", lastName[i],firstName[i], &ages[i]);
     }
-    bubbleSort(lastName, firstName, ages);
+    bubbleSort(lastName, firstName, ages, numberToList);
+
+    for (int i = 0; i < numberToList; i++) {
+        printf("%s %s %d\n", lastName[i], firstName[i], ages[i]);
+    }
 }
 
 
 //Leaving off here need to implement bubble sort on multiple arrays
-void bubbleSort(char lastName[ARRAYSIZE][50], char firstName[ARRAYSIZE][50], int age[]){
-    printf("%s", lastName[1]);
+void bubbleSort(char lastName[ARRAYSIZE][50], char firstName[ARRAYSIZE][50], int ages[], int numberToList){
+    int i, j, temp2;
+    char temp[50];
+    for (i = 0; i < numberToList - 1; i++) {
+        for (j = 0; j < numberToList - i - 1; j++) {
+            if(strcmp(lastName[j], lastName[j+1]) > 0){
+                strcpy(temp, lastName[j]);
+                strcpy(lastName[j], lastName[j+1]);
+                strcpy(lastName[j+1], temp);
+                strcpy(temp, firstName[j]);
+                strcpy(firstName[j], firstName[j+1]);
+                strcpy(firstName[j+1], temp);
+                temp2 = ages[j];
+                ages[j] = ages[j+1];
+                ages[j+1] = temp2;
+            }
+        }
+    }
 }
 
+void swap(int* i, int* j){
+    int temp = *i;
+    *i = *j;
+    *j = temp;
+}
 
