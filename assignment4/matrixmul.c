f0798b062491a1436e90040673d54f109f953249
CynicalMrJones <wopjr1234@gmail.com>
2024-06-13 15:30:13 -0400

    Added assignment 4

---
+++ assignment4/matrixmul.c
@@ -0,0 +1,35 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+//Given an n x k matrix A and an k x m matrix B, with 1 ≤ n, m,k ≤ 300, write a C program
+//that computes the matrix product C=AB. All entries in matrices A and B are integers
+//with absolute value less than 1000, so you don't need to worry about overflow. If
+//matrices A and B do not have the right dimensions to be multiplied, the product
+//matrix C should have its number of rows and columns both set to zero.
+int main(){
+    //First things first i need to get input the way written in the assignment
+    int n,k;
+    printf("Please input the size of matrix A (seperated by spaces): ");
+    scanf("%d %d",&n, &k);
+    //Now i need to create a matrix...trying to decide if i should use pointers or an array
+    int **ptr;
+    ptr = (int **)calloc(n,sizeof(int *));
+    ptr[0] = (int *)calloc(k,sizeof(int));
+    ptr[1] = (int *)calloc(n,sizeof(int));
+
+    //now i need to prompt the user to input n+1 entries
+    //Stopping here gonna try again tomorrow
+    for (int i = 0; i < n; i++) {
+        printf("Please input numbers: ");
+        for (int j = 0; j < k; j++) {
+            scanf("%d %d", &ptr[i][j]);
+        }
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < k; j++) {
+            printf("%d %d\n", ptr[i][j], ptr[i+1][j]);
+        }
+    }
+
+}
